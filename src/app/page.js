53ad6d02bcc22c4c25da711c5bd4232ddf9ace9e
Dennar David <dennardavid@gmail.com>
2024-08-21 23:34:30 +0100

    lets see

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Hero from "./components/hero";
 export default function Home() {
   return (
     <>
-      <main className="w-dvw bg-[url('/mobile.jpg')] md:bg-[url('/tablet.jpeg')] lg:bg-[url('/pixelcut-export.jpeg')] min-h-screen full bg-cover bg-no-repeat pb-3">
+      <main className="w-dvw bg-[url('/mobile.jpg')] md:bg-[url('/tablet.jpeg')] lg:bg-[url('/pixelcut-export.jpeg')] min-h-screen full bg-cover bg-no-repeat pb-1">
         <Navbar />
         <Hero />
       </main>
