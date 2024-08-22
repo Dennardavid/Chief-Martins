255d16fd1aabfb259feded9d5ed1e8e573ecdca6
Dennar David <dennardavid@gmail.com>
2024-08-22 19:17:22 +0100

    navbar animation

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,12 +1,49 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
-import { IoMenuSharp } from "react-icons/io5";
-import { IoCloseSharp } from "react-icons/io5";
+import { useState, useEffect, useRef } from "react";
+import { IoMenuSharp, IoCloseSharp } from "react-icons/io5";
+import gsap from "gsap";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const navbarRef = useRef(null);
+  const navItemsRef = useRef([]);
+
+  useEffect(() => {
+    if (menuOpen) {
+      gsap.to(navbarRef.current, {
+        x: 0,
+        duration: 0.5,
+        ease: "power3.inOut",
+      });
+      gsap.fromTo(
+        navItemsRef.current,
+        { x: 50, opacity: 0 },
+        {
+          x: 0,
+          opacity: 1,
+          stagger: 0.1,
+          duration: 0.5,
+          delay: 0.2,
+          ease: "power3.inOut",
+        }
+      );
+    } else {
+      gsap.to(navbarRef.current, {
+        x: "100%",
+        duration: 0.5,
+        ease: "power3.inOut",
+      });
+      gsap.to(navItemsRef.current, {
+        x: -50,
+        opacity: 0,
+        stagger: -0.1,
+        duration: 0.5,
+        ease: "power3.inOut",
+      });
+    }
+  }, [menuOpen]);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -20,41 +57,37 @@ export default function Navbar() {
         </Link>
 
         {/* Mobile Menus */}
-        {menuOpen && (
-          <div>
-            <ul className="fixed top-0 right-0 text-white bg-amber-950 backdrop-filter bg-opacity-30 backdrop-blur z-50 w-[80%] h-2/4 flex flex-col justify-center items-center gap-5 p-8 lg:hidden md:gap-5 xl:gap-10 md:text-base 2xl:text-xl">
-              <li>
-                <Link href="/order-of-events">Order of Events</Link>
+        <div
+          ref={navbarRef}
+          className="navbar fixed top-0 right-0 text-white bg-amber-950 backdrop-filter bg-opacity-30 backdrop-blur z-50 w-[80%] h-[55%] flex flex-col justify-center items-center gap-5 p-8 lg:hidden md:gap-5 xl:gap-10 md:text-base 2xl:text-xl"
+          style={{ transform: "translateX(100%)" }}
+        >
+          <ul className="list-none w-full flex flex-col gap-4 items-center">
+            {[
+              "/order-of-events",
+              "/direction",
+              "/accomodation",
+              "/logistics",
+              "/asoebi",
+              "/donation",
+              "/gallery",
+            ].map((href, index) => (
+              <li
+                key={index}
+                ref={(el) => (navItemsRef.current[index] = el)}
+                className="navItems"
+              >
+                <Link href={href}>{href.split("/")[1].replace(/-/g, " ")}</Link>
               </li>
-              <li>
-                <Link href="/direction">Direction</Link>
-              </li>
-              <li>
-                <Link href="/accomodation">Accomodation</Link>
-              </li>
-              <li>
-                <Link href="/logistics">Logistics</Link>
-              </li>
-              <li>
-                <Link href="/asoebi">Asoebi</Link>
-              </li>
-              <li>
-                <Link href="/donation">Donation</Link>
-              </li>
-              <li>
-                <Link href="/gallery">Gallery</Link>
-              </li>
-              <button onClick={toggleMenu} className="flex lg:hidden absolute top-4 right-4">
-              {menuOpen ? (
-                <IoCloseSharp size={35} />
-              ) : (
-                <IoMenuSharp size={35} />
-              )}
-            </button>
-            </ul>
-           
-          </div>
-        )}
+            ))}
+          </ul>
+          <button
+            onClick={toggleMenu}
+            className="flex lg:hidden absolute top-3 right-3 ease-in-out duration-500"
+          >
+            <IoCloseSharp size={35} />
+          </button>
+        </div>
 
         {/* Desktop Menus */}
         <ul className="hidden lg:flex md:gap-5 xl:gap-10 md:text-base 2xl:text-xl">
@@ -82,9 +115,8 @@ export default function Navbar() {
         </ul>
 
         <button onClick={toggleMenu} className="flex lg:hidden">
-          {menuOpen ? <IoCloseSharp size={35} /> : <IoMenuSharp size={35} />}
+          <IoMenuSharp size={35} />
         </button>
-        {/* <IoMenuSharp size={35} className="flex lg:hidden" /> */}
       </nav>
     </header>
   );
