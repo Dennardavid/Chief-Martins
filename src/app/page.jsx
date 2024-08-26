2125765bd939b50be77398755f2e6c7ca521e1ce
Dennar David <dennardavid@gmail.com>
2024-08-26 09:04:15 +0100

    object fit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/hero";
 export default function Home() {
   return (
     <>
-      <main className="w-dvw bg-[url('/mobile.jpeg')] md:bg-[url('/tablet.jpeg')] lg:bg-[url('/laptop.jpeg')] h-screen full bg-cover bg-no-repeat pb-1">
+      <main className="w-dvw bg-[url('/mobile.webp')] md:bg-[url('/tablet.webp')] lg:bg-[url('/laptop.webp')] h-screen full bg-cover bg-no-repeat pb-1">
         <div
           className="fixed inset-0 bg-black bg-opacity-50 z-40"
           id="wrapper"
