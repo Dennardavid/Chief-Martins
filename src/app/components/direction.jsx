66f0fc2951e6517927f492a608f52c09e56c750f
Dennar David <dennardavid@gmail.com>
2024-09-17 22:12:25 +0100

    gift page adjustment for mobile screen

--- src/app/components/direction.jsx
+++ src/app/components/direction.jsx
@@ -1,16 +1,40 @@
-export default async function Direction() {
+"use client"
 
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+
+export default function Direction() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoaded(true), 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div className="flex justify-center items-center mt-5">
-      <iframe
-        src="https://www.google.com/maps/d/embed?mid=1wAgqkNFAFKe4JEJPG4z0LqNzaL4VS0Y&ehbc=2E312F&noprof=1"
-        width="100%"
-        height="600"
-        allowFullScreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+    <div className="flex justify-center items-center mt-5 relative" style={{ height: '600px' }}>
+      {!isLoaded && (
+        <div className="absolute inset-0 flex items-center justify-center bg-gray-500">
+          <Image
+            src="/air-peace.png"
+            alt="Map loading placeholder"
+            width={800}
+            height={600}
+            className="object-cover"
+          />
+          <div className="absolute">Loading map...</div>
+        </div>
+      )}
+      {isLoaded && (
+        <iframe
+          src="https://www.google.com/maps/d/embed?mid=1wAgqkNFAFKe4JEJPG4z0LqNzaL4VS0Y&ehbc=2E312F&noprof=1"
+          width="100%"
+          height="600"
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      )}
     </div>
   );
 }
--- src/app/gifts/page.jsx
+++ src/app/gifts/page.jsx
@@ -6,15 +6,15 @@ export default function DonationPage() {
       <div className="w-dvw bg-[url('/gifts-mobile-bg.jpeg')] md:bg-[url('/gifts-tablet-bg.jpeg')] lg:bg-[url('/gifts-bg.jpeg')] h-screen full bg-cover bg-no-repeat">
         <div className="fixed inset-0 bg-black bg-opacity-50 z-40" id="wrapper">
           <Navbar />
-          <section className="relative top-[30%] lg:top-[29%] lg:left-[2.5%] flex flex-col mx-auto lg:mx-0 gap-3 px-2 md:px-8 lg:px-10 justify-center items-center mt-7 text-white backdrop-blur-md backdrop-filter bg-white/10 w-[400px] md:w-[650px] p-4 rounded-md shadow-md shadow-white/20">
-            <p className="text-sm md:text-base 2xl:text-xl w-[370px] md:w-[600px]">
+          <section className="relative top-[30%] lg:top-[29%] lg:left-[2.5%] flex flex-col mx-auto lg:mx-0 gap-3 px-2 md:px-8 lg:px-10 justify-center items-center mt-7 text-white backdrop-blur-md backdrop-filter bg-amber-950/20 w-[96%] md:w-[650px] p-4 rounded-md shadow-md shadow-white/20">
+            <p className="text-sm md:text-base 2xl:text-xl w-[96%] md:w-[600px]">
               We sincerely appreciate your thoughts, prayers, and support during
               this difficult time. Your kind words and prayers have brought us
               great comfort, and we are deeply grateful. For those who would
               like to give a gift in memory, we kindly ask that you use the
               designated channel below. Thank you for your love and support.
             </p>
-            <div className="flex items-center gap-4 bg-amber-950/30 p-2 rounded-md shadow-md w-[370px] md:w-[600px]">
+            <div className="flex items-center gap-4 bg-white/20 p-2 rounded-md shadow-md w-[96%] md:w-[600px]">
               <img
                 src="/wema.png"
                 alt="wema bank"
@@ -25,7 +25,7 @@ export default function DonationPage() {
                 <span>Wema bank Naira account</span>
               </p>
             </div>
-            <div className="flex items-center gap-4 bg-amber-950/30 p-2 rounded-md shadow-md w-[370px] md:w-[600px]">
+            <div className="flex items-center gap-4 bg-white/20 p-2 rounded-md shadow-md w-[96%] md:w-[600px]">
               <img
                 src="/wema.png"
                 alt="wema bank"
