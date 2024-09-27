8aa800af6cd92f7612b6e719ca48920f0fccf15d
Dennar David <dennardavid@gmail.com>
2024-09-27 20:12:25 +0100

    Open graph image test

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
-import {  Geologica } from "next/font/google";
+import { Geologica } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
-
 
 const inter = Geologica({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -15,6 +15,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href={metadata.icons.icon} />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+        />
+        <meta property="og:title" content={metadata.description} />
+        <meta property="og:description" content="Join us to Celebrate the Life of Chief Martins Dafe Abu" />
+        <meta
+          property="og:image"
+          content="https://asset.cloudinary.com/dsngkhlct/bd08623839163f963adc1322bc5a69dc"
+        />
+        <meta property="og:url" content="https://chief-martins-dafe.netlify.app/" />
+        <meta property="og:type" content="website" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
